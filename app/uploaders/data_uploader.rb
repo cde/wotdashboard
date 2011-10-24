95e9961c9e25a3dfb5a6d8f0a013125558134b4b
cde <cdechauri@gmail.com>
2011-10-24 15:04:22 -0700

    added provisional store_dir

--- app/uploaders/data_uploader.rb
+++ app/uploaders/data_uploader.rb
@@ -14,7 +14,11 @@ class DataUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "data"
+    if Rails.env == 'development'
+      'data'
+    else
+     'tmp/data'
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
