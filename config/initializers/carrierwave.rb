23b20067f75c0dfc9165361c4ee22e1bb6eceead
Miguel Godoy <miguelgodoyg@gmail.com>
2011-11-14 21:47:06 -0300

    Real Fix… for Space and Chart date
    - Change the "style" in home.html.haml
    - Change format of date in javascript

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,17 @@
+
 unless Kernel.const_defined?("S3_CONFIG")
   S3_CONFIG = YAML.load_file("#{Rails.root}/config/fog_credentials.yml")[Rails.env].try(:symbolize_keys)
 end
 
 puts S3_CONFIG.inspect
 #Heroku has a read-only filesystem, so uploads must be stored on S3 and cannot be cached in the public directory.
-#Until we set S3 we're gonna set cache_dir'
-<<<<<<< HEAD
+#Until we set S3 we're gonna set cache_dir
+#<<<<<<< HEAD
 #CarrierWave.configure do |config|
 #  config.root = Rails.root.join('tmp')
 #  config.cache_dir = 'carrierwave'
 #end
-=======
+#=======
 CarrierWave.configure do |config|
   #config.root = Rails.root.join('tmp')
   #config.cache_dir = 'carrierwave'
@@ -28,4 +29,4 @@ CarrierWave.configure do |config|
 
 end
 
->>>>>>> 728dfcffc7adf20dc9d524224b22203caebe5d7e
+#>>>>>>> 728dfcffc7adf20dc9d524224b22203caebe5d7e
