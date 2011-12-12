dc6a3763b53d7737d315583004055538c4f06401
Miguel Godoy <miguelgodoyg@gmail.com>
2011-12-12 17:21:27 -0300

    New routes modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wotdashboard::Application.routes.draw do
 
+  resources :reports
+
   match "funnels" => "funnels#index", :as => :funnels
   match "funnels/normal/:start/:end/:traffic_type" => "funnels#get_data_normal", :as => :funnels_get_data_normal
   match "funnels/adquisition/:start/:end/:traffic_type" => "funnels#get_data_adquisition", :as => :funnels_get_data_adquisition
