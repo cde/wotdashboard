4e538290ae956bd0f01872a8f975655fb459c9ae
cde <cdechauri@gmail.com>
2011-12-13 00:58:05 -0800

    Some styling & added totals to report show page

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,6 @@ require 'iconv'
 
 class Report < ActiveRecord::Base
   belongs_to :graphic
-
   has_many :funnels
 
   attr_accessible :starts_date, :ends_date, :title, :note, :data, :graphic_id
@@ -11,10 +10,9 @@ class Report < ActiveRecord::Base
 
   mount_uploader :data, DataUploader
 
-  scope :ads, where(traffic_type: "Ads").order("start_date asc")
-  scope :direct, where(traffic_type: "Direct").order("start_date asc")
 
 
+  after_create :set_default_graphic
 
   def title_slugize(slug = '-')
     sluged = Iconv.iconv('ascii//TRANSLIT//IGNORE', 'utf-8', self.title).to_s
@@ -23,6 +21,10 @@ class Report < ActiveRecord::Base
     sluged.gsub!(/#{slug}{2,}/i, slug)
     sluged.gsub!(/^#{slug}|#{slug}$/i, '')
     URI.escape(sluged.downcase, /[^\w_+-]/i)
+  end
+
+  def set_default_graphic
+    self.graphic = Graphic.find_by_chart_type("adquisition")
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,33 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-=begin
-if User.all.empty?
-  puts 'SETTING UP DEFAULT USER LOGIN'
-  user = User.create! :username => 'jeremy', :email => 'jeremy@test.com', :password => 'password', :password_confirmation => 'password'
-  puts 'New user created: ' << user.name
-end
-=end
+#=begin
+#if User.all.empty?
+#  puts 'SETTING UP DEFAULT USER LOGIN'
+#  user = User.create! :username => 'jeremy', :email => 'jeremy@test.com', :password => 'password', :password_confirmation => 'password'
+#  puts 'New user created: ' << user.name
+#end
+#=end
+#
+#=begin
+#if Funnel.all.empty? && Rails.env == 'development'
+# start_time, end_time = nil
+#  (1..36).each do |i|
+#    if start_time.nil? && end_time.nil?
+#      start_time = DateTime.new(2011, 9, 05)
+#      end_time = start_time + 4.day
+#    else
+#      start_time = start_time + 1.week
+#      end_time = start_time + 4.day
+#    end
+#    Funnel.create(traffic_type: 'Direct', confirmed: 10000 * i, logged_in: (10000 * i) * 0.8, first_battle: (10000 * i) * 0.6,first_upgrade: (10000 * i) * 0.4, tank_purchase: (10000 * i) * 0.2, start_date: start_time, end_date: end_time )
+#    Funnel.create(traffic_type: 'Ads', confirmed: 5000  * i, logged_in: (5000  * i) * 0.8, first_battle: (5000  * i) * 0.6, first_upgrade: (5000  * i) * 0.4, tank_purchase: (5000  * i) * 0.2, start_date: start_time, end_date: end_time )
+#  end
+#end
+#=end
 
-=begin
-if Funnel.all.empty? && Rails.env == 'development'
- start_time, end_time = nil
-  (1..36).each do |i|
-    if start_time.nil? && end_time.nil?
-      start_time = DateTime.new(2011, 9, 05)
-      end_time = start_time + 4.day
-    else
-      start_time = start_time + 1.week
-      end_time = start_time + 4.day
-    end
-    Funnel.create(traffic_type: 'Direct', confirmed: 10000 * i, logged_in: (10000 * i) * 0.8, first_battle: (10000 * i) * 0.6,first_upgrade: (10000 * i) * 0.4, tank_purchase: (10000 * i) * 0.2, start_date: start_time, end_date: end_time )
-    Funnel.create(traffic_type: 'Ads', confirmed: 5000  * i, logged_in: (5000  * i) * 0.8, first_battle: (5000  * i) * 0.6, first_upgrade: (5000  * i) * 0.4, tank_purchase: (5000  * i) * 0.2, start_date: start_time, end_date: end_time )
-  end
-end
-=end
-
-if Graphic.all.empty? && Rails.env == 'development'
+if Graphic.all.empty?
+#&& Rails.env == 'development'
   Graphic.create(title: 'Adquisitions', description: 'Description #1', :chart_type => 'adquisition')
   Graphic.create(title: 'Bars', description: 'Description #2', :chart_type => 'column')
   Graphic.create(title: 'Lines', description: 'Description #3', :chart_type => 'line')
